ef720dd695e04ad2a408b2b2419e58ba3bc7fd12
khoivd <vudangkhoi1994@gmail.com>
2020-01-02 16:28:32 +0700

    fix getAllWord endpoint

--- src/controllers/word.js
+++ src/controllers/word.js
@@ -27,8 +27,8 @@ async function getWordById(req, res) {
 
 async function getAllWord(req, res) {
     try {
-        const { limit, offset } = req.query;
-        const words = await Word.find({}).limit(limit || 10).offset(offset || 0);
+        const { limit, skip } = req.query;
+        const words = await Word.find({}).limit(limit || 10).skip(skip || 0);
         if (!words) {
             return res.status(404).send({ message: 'No word found!' })
         }
